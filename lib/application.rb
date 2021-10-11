bccf8988587ee424c80a284a296f7a0693cbf22a
MishaDak <dack.misha@gmail.com>
2021-10-11 23:39:30 +0700

    Update application

--- lib/application.rb
+++ lib/application.rb
@@ -1,22 +1,23 @@
 require_relative 'temperature_converter'
 
+@types = %w[C c K k F f]
 loop do
-  @builder = TemperatureConverter.new
+  builder = TemperatureConverter.new
   puts 'Enter the units you want to convert from (C, K, F): '
-  @from = $stdin.gets.chomp
+  from = $stdin.gets.chomp until @types.include?(from)
 
   puts 'Enter the value: '
-  @value = $stdin.gets.chomp
+  value = $stdin.gets.to_f
 
   puts 'Enter the units you want to convert to (C, K, F): '
-  @to = $stdin.gets.chomp
+  to = $stdin.gets.chomp until @types.include?(to) && (to != from)
 
-  @result = @builder.type_set(@from)
-                    .value_set(@value)
-                    .convert_to(@to)
+  result = builder.type_set(from)
+                  .value_set(value)
+                  .convert_to(to)
 
-  puts "Convert from #{@from} to #{@to} result: #{@result}"
+  puts "Convert from #{from} to #{to} result: #{result}"
   puts 'enter yes if want quit'
-  @quit = $stdin.gets.chomp
-  break if @quit == 'yes'
+  quit = $stdin.gets.chomp
+  break if quit == 'yes'
 end
